fab94eec70d1007bc9edbba715a408e4320b270d
Hope <hopegiometti@gmail.com>
2019-12-30 18:46:26 -0500

    added card features

--- src/PodcastCard.js
+++ src/PodcastCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Segment } from 'semantic-ui-react'
 
 
 export default class PodcastCard extends React.Component {
@@ -7,13 +7,24 @@ export default class PodcastCard extends React.Component {
       this.props.editFavs(this.props.podcast)
     }
 
+    seeMorePodInfo = () => {}
+
     render() {
       // console.log(this.props.podcast.podcast.title)
-        return (<div>
+        return (<div onClick={this.seeMorePodInfo} >
         <div className="ui link cards">
            <div className="card">
-            <div className="image" >
-              <img src={this.props.podcast.thumbnail} alt=""/> 
+             <div className="ui slide reveal">
+              {/* <div className="visible content" > */}
+                <img src={this.props.podcast.thumbnail} height={300} alt="" className="visible content"/> 
+              {/* </div> */}
+              <div className="hidden content" >
+              <Segment style={{overflow: 'auto', maxHeight: 200 }}>{this.props.podcast.description}</Segment>
+                <p>Genre: {this.props.podcast.genre} </p>
+                <button className="ui icon button" >
+                  <i className="external url icon" ></i>
+                </button>
+              </div>
             </div>
             <div className="content">
               <div className="header">{this.props.podcast.title}</div>
