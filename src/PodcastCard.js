37af71c5d13084e3014e4a9b82b52d24ecfef46d
Hope <hopegiometti@gmail.com>
2019-12-30 19:14:54 -0500

    added label

--- src/PodcastCard.js
+++ src/PodcastCard.js
@@ -16,16 +16,19 @@ export default class PodcastCard extends React.Component {
         return (<div >
         <div className="ui link cards">
            <div className="card">
-             <div className="ui slide reveal">
-                <img src={this.props.podcast.thumbnail} height={300} alt="" className="visible content"/> 
-              <div className="hidden content" >
-              <Segment style={{overflow: 'auto', maxHeight: 200 }}>{this.props.podcast.description}</Segment>
-                <p>Genre: {this.props.podcast.genre} </p>
-                <button className="ui icon button" onClick={this.linkToWebsite}>
-                  <i className="external url icon" ></i>
-                </button>
+            {/* <div class="ui raised segment"> */}
+              <div className="ui slide reveal">
+                  <img src={this.props.podcast.thumbnail} height={300} alt="" className="visible content"/> 
+                <div className="hidden content" >
+                  {this.props.podcast.explicit_content ? <a className="ui red horizontal label">Explicit</a> : null}
+                <Segment style={{overflow: 'auto', maxHeight: 200 }}>{this.props.podcast.description}</Segment>
+                  <p>Genre: {this.props.podcast.genre} </p>
+                  <button className="ui icon button" onClick={this.linkToWebsite}>
+                    <i className="external url icon" ></i>
+                  </button>
+                </div>
               </div>
-            </div>
+            {/* </div> */}
             <div className="content">
               <div className="header">{this.props.podcast.title}</div>
             </div>
